fae642f269777b9dbf40037d4c26ac21945571e7
Leonardo Cordeiro <leonardobcordeirodev@gmail.com>
2017-04-14 12:08:13 -0300

    minors

--- app/src/main/java/org/lema/notasapp/infra/firebase/notification/PushService.java
+++ app/src/main/java/org/lema/notasapp/infra/firebase/notification/PushService.java
@@ -3,15 +3,10 @@ package org.lema.notasapp.infra.firebase.notification;
 import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
-import android.content.Context;
 import android.content.Intent;
-import android.graphics.Color;
-import android.media.RingtoneManager;
-import android.net.Uri;
-import android.os.Vibrator;
 import android.support.v4.app.NotificationCompat;
-import android.support.v4.app.NotificationManagerCompat;
 
+import com.google.firebase.messaging.FirebaseMessaging;
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
 
@@ -25,25 +20,40 @@ import org.lema.notasapp.ui.activity.FeedActivity;
 public class PushService extends FirebaseMessagingService {
 
     @Override
-    public void onMessageReceived(RemoteMessage remoteMessage) {
+    public void onCreate() {
+        super.onCreate();
 
-        Vibrator v = (Vibrator) this.getSystemService(Context.VIBRATOR_SERVICE);
+        FirebaseMessaging.getInstance().subscribeToTopic("feed");
+    }
+
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+        String title = remoteMessage.getData().get("title");
+        String message = remoteMessage.getData().get("message");
+
+        Notification notification = buildWith(title, message);
+        show(notification);
+
+    }
+
+    private void show(Notification notification) {
+        NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        notificationManager.notify(0, notification);
+    }
+
+    private Notification buildWith(String title, String text) {
         NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this);
 
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 1,
+                                                new Intent(this, FeedActivity.class),
+                                                PendingIntent.FLAG_CANCEL_CURRENT);
+
+
         notificationBuilder.setAutoCancel(true);
-        notificationBuilder.setContentTitle(remoteMessage.getData().get("title"));
-
-        //trocar pra message (ou de acordo com o que for feito na api)
-        notificationBuilder.setContentText(remoteMessage.getData().get("title"));
-        notificationBuilder.setSmallIcon(R.drawable.ic_status_notify_news);
-
-        Intent irParaFeed = new Intent(this, FeedActivity.class);
-        PendingIntent pendingIntent = PendingIntent.getActivity(this, 1, irParaFeed, PendingIntent.FLAG_CANCEL_CURRENT);
-
+        notificationBuilder.setContentTitle(title);
+        notificationBuilder.setContentText(text);
         notificationBuilder.setContentIntent(pendingIntent);
-
-        Uri alarmSound = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
-        notificationBuilder.setSound(alarmSound);
+        notificationBuilder.setSmallIcon(R.drawable.ic_status_notify_news);
 
         Notification notification = notificationBuilder.build();
 
@@ -51,9 +61,6 @@ public class PushService extends FirebaseMessagingService {
         notification.defaults|= Notification.DEFAULT_LIGHTS;
         notification.defaults|= Notification.DEFAULT_VIBRATE;
 
-        NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
-        notificationManager.notify(0, notification);
-
-
+        return notification;
     }
 }
