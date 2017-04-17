ce6649501b584cf06b7628cc183ed16629f395d8
IgorRamosBR <igorrpestana@yahoo.com.br>
2017-04-17 17:55:03 -0300

    Cores aleatorias no feed

--- app/src/main/java/org/lema/notasapp/viewholder/PostViewHolderSemImagem.java
+++ app/src/main/java/org/lema/notasapp/viewholder/PostViewHolderSemImagem.java
@@ -4,15 +4,14 @@ import android.app.Activity;
 import android.content.Intent;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import org.lema.notasapp.R;
 import org.lema.notasapp.domain.model.Post;
 import org.lema.notasapp.ui.activity.PostActivity;
 
-import java.text.DateFormat;
 import java.text.SimpleDateFormat;
-import java.util.Date;
 
 /**
  * Created by igor on 08/03/17.
@@ -21,18 +20,23 @@ import java.util.Date;
 public class PostViewHolderSemImagem extends RecyclerView.ViewHolder {
 
     private Activity activity;
+    private Integer layoutColor;
 
+    final RelativeLayout layout;
     final TextView mensagem;
     final TextView autor;
     final TextView data;
 
-    public PostViewHolderSemImagem(Activity activity, View itemView) {
+    public PostViewHolderSemImagem(Activity activity, View itemView, Integer layoutColor) {
         super(itemView);
 
         this.activity = activity;
+        this.layoutColor = layoutColor;
         mensagem = (TextView) itemView.findViewById(R.id.tv_feed_mensagem);
         autor = (TextView) itemView.findViewById(R.id.tv_feed_autor);
         data = (TextView) itemView.findViewById(R.id.tv_feed_data);
+        layout = (RelativeLayout) itemView.findViewById(R.id.rl_sem_foto);
+
     }
 
     public void onBind(final Post post) {
@@ -50,8 +54,12 @@ public class PostViewHolderSemImagem extends RecyclerView.ViewHolder {
             public void onClick(View view) {
                 Intent intent = new Intent(activity, PostActivity.class);
                 intent.putExtra("post", post);
+                intent.putExtra("color", layoutColor);
                 activity.startActivity(intent);
             }
         });
+
+        layout.setBackgroundResource(layoutColor);
     }
+
 }
